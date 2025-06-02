0b1efebaf4b49613af270ccf465d6c5246cedce0
Reeen <r_takahashi@yumemi.co.jp>
2025-06-02 22:27:37 +0900

    refactor: Replace with `FractionallySizedBox`

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -5,72 +5,53 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _ImageContent(size: screenSize.width / 2),
-                _TemperatureLabelContent(labelWidth: screenSize.width / 4),
-              ],
-            ),
+    return const Scaffold(
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              AspectRatio(aspectRatio: 1, child: Placeholder()),
+              _TemperatureLabelContent(),
+              Expanded(
+                child: Column(
+                  children: [SizedBox(height: 80), _ReloadButton()],
+                ),
+              ),
+            ],
           ),
-          Positioned(
-            bottom: 80,
-            left: 0,
-            right: 0,
-            child: _ReloadButton(buttonWidth: screenSize.width / 4),
-          ),
-        ],
+        ),
       ),
     );
   }
 }
 
-class _ImageContent extends StatelessWidget {
-  const _ImageContent({required double size}) : _size = size;
-  final double _size;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: _size,
-      height: _size,
-      child: const Placeholder(),
-    );
-  }
-}
-
 class _TemperatureLabelContent extends StatelessWidget {
-  const _TemperatureLabelContent({required double labelWidth}) : _labelWidth = labelWidth;
-  final double _labelWidth;
+  const _TemperatureLabelContent();
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-  
+
     return Padding(
       padding: const EdgeInsets.only(top: 16, bottom: 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
-            width: _labelWidth,
+          Expanded(
             child: Center(
               child: Text(
-                '**℃',
+                '** ℃',
                 style: theme.textTheme.labelLarge?.copyWith(color: Colors.blue),
               ),
             ),
           ),
-          SizedBox(
-            width: _labelWidth,
+          Expanded(
             child: Center(
               child: Text(
-                '**℃',
+                '** ℃',
                 style: theme.textTheme.labelLarge?.copyWith(color: Colors.red),
               ),
             ),
@@ -82,28 +63,29 @@ class _TemperatureLabelContent extends StatelessWidget {
 }
 
 class _ReloadButton extends StatelessWidget {
-  const _ReloadButton({required double buttonWidth}) : _buttonWidth = buttonWidth;
-  final double _buttonWidth;
+  const _ReloadButton();
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        SizedBox(
-          width: _buttonWidth,
-          child: TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(foregroundColor: Colors.blue),
-            child: const Text('Close'),
+        Expanded(
+          child: Center(
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(foregroundColor: Colors.blue),
+              child: const Text('Close'),
+            ),
           ),
         ),
-        SizedBox(
-          width: _buttonWidth,
-          child: TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(foregroundColor: Colors.blue),
-            child: const Text('Reload'),
+        Expanded(
+          child: Center(
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(foregroundColor: Colors.blue),
+              child: const Text('Reload'),
+            ),
           ),
         ),
       ],
