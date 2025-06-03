5898e34fadd2ee6a319bbec48604d6baae643160
Reeen <r_takahashi@yumemi.co.jp>
2025-06-03 17:49:43 +0900

    feat: Add navigation to mainscreen from launchscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/screens/main_screen.dart';
+import 'package:flutter_training/screens/launch_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,6 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MainScreen());
+    return const MaterialApp(home: LaunchScreen());
   }
 }
---
+++ lib/screens/launch_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_training/screens/main_screen.dart';
+
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({super.key});
+
+  @override
+  State<LaunchScreen> createState() => _LaunchScreenState();
+}
+
+class _LaunchScreenState extends State<LaunchScreen> {
+  @override
+  void initState() {
+    super.initState();
+    unawaited(
+      WidgetsBinding.instance.endOfFrame.then((_) async {
+        await Future<void>.delayed(const Duration(milliseconds: 500));
+        _presentMainScreen();
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+
+  void _presentMainScreen() {
+    unawaited(
+      Navigator.of(
+        context,
+      ).push(MaterialPageRoute(builder: (context) => const MainScreen())),
+    );
+  }
+}
