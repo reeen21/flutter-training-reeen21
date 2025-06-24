ed6b07119afbc661ee6adb2bca07a84a40bf7bb8
Reeen <r_takahashi@yumemi.co.jp>
2025-06-24 13:23:01 +0900

    refactor: wrap YumemiWeatherRequest test in group

--- test/entity/yumemi_weather_request_test.dart
+++ test/entity/yumemi_weather_request_test.dart
@@ -2,14 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_training/entity/yumemi_weather_request.dart';
 
 void main() {
-  test('JSONにエンコードできること', () {
-    final date = DateTime(2025, 6, 19);
-    final request = YumemiWeatherRequest(
-      area: 'tokyo',
-      date: date,
-    );
-    final json = request.toJson();
-    expect(json['area'], 'tokyo');
-    expect(json['date'], date.toIso8601String());
+  group('YumemiWeatherRequest', () {
+    test('JSONにエンコードできること', () {
+      final date = DateTime(2025, 6, 19);
+      final request = YumemiWeatherRequest(
+        area: 'tokyo',
+        date: date,
+      );
+      final json = request.toJson();
+      expect(json['area'], 'tokyo');
+      expect(json['date'], date.toIso8601String());
+    });
   });
 }
